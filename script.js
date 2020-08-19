093a5f17a309478310ef826e90cc0b4843d20c53
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-19 14:18:03 +0100

    components in file

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-new Vue({
-    el: "#app",
-    data() {
-        return {
-            firstname: '',
-            lastname: '',
-            students: [{
-                firstname: "Afolabi",
-                lastname: "Opakunle"
-            }]
+Vue.component("idea", {
+    component: "<li>Develop another Facebook website</li>"
+})
 
-        }
-    },
-    methods: {
-        addStudent() {
-            student = {
-                firstname: firstname.value,
-                lastname: lastname.value
-            }
-            this.students.push(student);
-            this.firstname = '';
-            this.lastname = ''
-        }
-    }
+new Vue({
+    el: "#app"
 })
