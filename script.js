ec38c18cbf8aa22f5be82388917010faa7733454
afolabiopakunle <opakunleafolabi@gmail.com>
2020-08-19 11:47:23 +0100

    VueJS added

---
+++ script.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: "#app",
+    data() {
+        return {
+            firstname: '',
+            lastname: '',
+            students: [{
+                firstname: "Afolabi",
+                lastname: "Opakunle"
+            }]
+
+        }
+    },
+    methods: {
+        send() {
+
+        }
+    }
+})
